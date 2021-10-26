755f868bce6dcf1a18964e00b828042ec853565d
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2021-10-25 23:33:40 -0500

    header/hero plus moment and setInterval()

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+//displays time and date using moment.js() with setInterval()
+
+var datetime = null,
+    date = null;
+
+var update = function() {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+}
+
+$(document).ready(function(){
+    datetime = $('#datetime')
+    update();
+    setInterval(update,1000);
+})
